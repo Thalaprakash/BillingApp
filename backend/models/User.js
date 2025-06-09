96b3bd93acc947a9f1718ac6e25100953fb469fa
Thalaprakash <prakashraja1410@gmail.com>
2025-06-09 13:07:01 +0530

    ad

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  phone: String,
-  password: String,
-  role: { type: String, default: 'user' },
-  active: { type: Boolean, default: true },
-});
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  active: { type: Boolean, default: true }
+}, { timestamps: true });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
