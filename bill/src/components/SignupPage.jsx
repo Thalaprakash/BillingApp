ae2c0d350d612992150ed0e4d8d14966f021fb22
Thalaprakash <98447686+Thalaprakash@users.noreply.github.com>
2025-06-25 16:45:41 +0530

    Update SignupPage.jsx

--- bill/src/components/SignupPage.jsx
+++ bill/src/components/SignupPage.jsx
@@ -21,7 +21,7 @@ export default function SignupPage({ setUser }) {
     }
 
     try {
-      const res = await axios.post('http://localhost:3001/api/auth/signup', form);
+      const res = await axios.post('https://enterprisesmdu.com/api/auth/signup', form);
       const userData = res.data.user;
 
       if (userData) {
